b74f97342a889958b954f17870a7cc4907d2f16b
eternityid <son.nguyen.k2set@gmail.com>
2017-10-13 10:40:06 +0700

    13/10/2017

---
+++ Interns2.Domain/Enum/FilmType.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Interns2.Domain.Enum
+{
+    public enum FilmType
+    {
+        HanhDong,
+        TinhCam,
+        HoatHinh,
+        KinhDi,
+        PhieuLuu,
+        VienTuong,
+        Hai,
+        None
+    }
+}
