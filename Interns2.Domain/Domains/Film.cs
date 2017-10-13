d36fb1d49ae486ae1ab61558f29d61f6ae728a62
HUY_TRAN <huytranprers@gmail.com>
2017-10-13 11:07:31 +0700

    Add 3 file

---
+++ Interns2.Domain/Domains/Film.cs
@@ -0,0 +1,14 @@
+﻿using Interns2.Domain.Enum;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Interns2.Domain.Domains
+{
+    class Film
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public List<FilmType> Types { get; set; }
+    }
+}
---
+++ Interns2.Domain/Enum/FilmType.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Interns2.Domain.Enum
+{
+    public enum FilmType
+    {
+        HanhDong,
+        Tinhcam,
+        HoatHinh,
+        KinhDi,
+        PhieuLuu,
+        VienTuong,
+        Hai,
+        None
+    }
+}
