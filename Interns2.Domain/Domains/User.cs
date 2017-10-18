f00815d4eaa85bf48bdadf5297e6fefc5f613fea
DESKTOP-NE70A7B\Mỹ Linh <loganmai2805@gmail.com>
2017-10-18 11:16:13 +0700

    update code for user model

---
+++ Interns2.Domain/Domains/User.cs
@@ -0,0 +1,18 @@
+﻿using Interns2.Infrastructure.MongoDb.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Interns2.Domain.Domains
+{
+    public class User : AuditableEntityBase, IAggregateRoot
+    {
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+        public string Fullname { get; set; }
+
+        public string Address { get; set; }
+    }
+}
