ebd663fa46693415fca5d492c9a5d77a180576a1
HUY_TRAN <huytranprers@gmail.com>
2017-10-18 14:22:30 +0700

    update data annotation film file

--- Interns2.Domain/Domains/Film.cs
+++ Interns2.Domain/Domains/Film.cs
@@ -12,26 +12,33 @@ namespace Interns2.Domain.Domains
     [BsonIgnoreExtraElements]
     public class Film : AuditableEntityBase, IAggregateRoot
     {
-        [Required]
+        [Required(ErrorMessage = "Title is required")]
+        [StringLength(100)]
         public string Title { get; set; }
-        
+
+        [StringLength(360)]
         public string Description { get; set; }
 
         public List<FilmType> Types { get; set; } = new List<FilmType>();
 
+        [Required(ErrorMessage = "Date is required")]
         public DateTime? DatePublish { get; set; }
 
         public string Producer { get; set; }
 
+        [Range(1, 10, ErrorMessage = "Rate must be between 1 and 10")]
         [BsonRepresentation(BsonType.Int64, AllowTruncation = true)]
         public int Rate { get; set; }
 
+        [Required(ErrorMessage = "Image is required")]
         public string Image { get; set; }
 
+        [Required(ErrorMessage = "LinkFilm is required")]
         public string LinkFilm { get; set; }
 
         public List<string> Actors { get; set; } = new List<string>();
 
         public FilmStatus Status { get; set; }
+
     }
 }
