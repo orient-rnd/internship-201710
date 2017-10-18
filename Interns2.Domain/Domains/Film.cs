cc1171c1c3d20413f73b8e84132036019a1fe664
Duc Pham <phduc95@gmail.com>
2017-10-18 11:28:10 +0700

    validation firms

--- Interns2.Domain/Domains/Film.cs
+++ Interns2.Domain/Domains/Film.cs
@@ -3,6 +3,7 @@ using Interns2.Infrastructure.MongoDb.Models;
 using MongoDB.Bson.Serialization.Attributes;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace Interns2.Domain.Domains
@@ -10,18 +11,26 @@ namespace Interns2.Domain.Domains
     [BsonIgnoreExtraElements]
     public class Film : AuditableEntityBase, IAggregateRoot
     {
+        [StringLength(100)]
+        [Required]
         public string Title { get; set; }
 
+        [StringLength(360)]
+        [Required]
         public string Description { get; set; }
 
         public List<FilmType> Types { get; set; } = new List<FilmType>();
 
         public FilmType Type { get; set; }
+        
 
         public DateTime? DatePublish { get; set; }
 
+        [RegularExpression(@"^[A-Z]+[a-zA-Z''-'\s]*$")]
+        [Required]
         public string Producer { get; set; }
 
+        [Range(0, 10)]
         public int Rate { get; set; }
 
         public string Image { get; set; }
