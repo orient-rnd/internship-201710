7d23b98e4748fdf45067d0d9c5787ee405fade01
DESKTOP-NE70A7B\Mỹ Linh <loganmai2805@gmail.com>
2017-10-18 16:05:18 +0700

    update

--- Interns2.AppServices.API/Controllers/UsersController.cs
+++ Interns2.AppServices.API/Controllers/UsersController.cs
@@ -3,14 +3,52 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Interns2.Domain.Domains;
+using MongoDB.Driver;
+using Interns2.Infrastructure.MongoDb;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace Interns2.AppServices.API.Controllers
 {
+    [Route("api/[controller]")]
     public class UsersController : Controller
     {
-        public IActionResult Index()
+        private readonly IMongoDbWriteRepository _writeRepository;
+
+        // GET: api/values
+        [HttpGet]
+        public IActionResult Get()
         {
-            return View();
+            var filter = Builders<User>.Filter.Empty;
+            var users = _writeRepository.Find(filter).ToList();
+
+            return Ok(users);
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
         }
     }
 }
