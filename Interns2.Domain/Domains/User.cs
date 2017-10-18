7acafff8b158e91acd5c39937f2cc248036566fa
Nguyen Ngoc Anh Thu <anhthunn120@gmail.com>
2017-10-18 16:06:49 +0700

    users validation

--- Interns2.Domain/Domains/User.cs
+++ Interns2.Domain/Domains/User.cs
@@ -10,10 +10,12 @@ namespace Interns2.Domain.Domains
     {
         [Required(ErrorMessage = "Email is required")]
         [DataType(DataType.EmailAddress)]
+        [MaxLength(50)]
+        [RegularExpression(@"[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}", ErrorMessage = "Please enter correct email")]
         public string Email { get; set; }
 
         [Required(ErrorMessage = "Password is required")]
-
+        [RegularExpression(@"(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@#$!%*?&])[A-Za-z\d#$@$!%*?&]{8,16}", ErrorMessage = "Password is not accepted")]
         public string Password { get; set; }
 
         [Required(ErrorMessage = "Full name is required")]
