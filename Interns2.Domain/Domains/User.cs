c820703a66abe1c287e8285f5bc100f44501364e
HUY_TRAN <huytranprers@gmail.com>
2017-10-18 14:33:19 +0700

    update data annotation for user file

--- Interns2.Domain/Domains/User.cs
+++ Interns2.Domain/Domains/User.cs
@@ -1,16 +1,24 @@
 ﻿using Interns2.Infrastructure.MongoDb.Models;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace Interns2.Domain.Domains
 {
     public class User : AuditableEntityBase, IAggregateRoot
     {
+        [RegularExpression(@"[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}", ErrorMessage = "Please enter correct email")]
+        [Required(ErrorMessage = "User Email is required")]
         public string Email { get; set; }
 
+        [Required(ErrorMessage = "User Passwork is required")]
+        [RegularExpression(@"[a-zA-Z0-9._%+-]", ErrorMessage = "Please enter Passwork correct")]
         public string Password { get; set; }
 
+
+        [Required(ErrorMessage = "User Name is required")]
+        [StringLength(20, MinimumLength = 5)]
         public string Fullname { get; set; }
 
         public string Address { get; set; }
